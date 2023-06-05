fe9d3a3b5573986623c8d6d0d1245279c753b0cf
Suzanne Tordjman <suzannetor@gmail.com>
2023-06-05 19:31:10 -0400

    still need some final touches

---
+++ script.js
@@ -0,0 +1,9 @@
+const buttonShare = document.getElementById("button__share")
+const socialShare = document.getElementById("social__share__container")
+
+
+buttonShare.addEventListener("click", ()=> {
+  socialShare.classList.toggle("hidden")
+
+
+})
